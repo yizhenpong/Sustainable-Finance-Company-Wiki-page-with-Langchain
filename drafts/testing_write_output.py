b6d4903da4da708cc582206d8c324f45293fe2e3
yizhenpong <yizhenpong@gmail.com>
2024-01-07 17:12:12 +0800

    cleaned version

--- drafts/testing_write_output.py
+++
@@ -1,7 +0,0 @@
-from data_helper import write_output
-outline = ['1','2','3 some kind of long outline in list form']
-write_output("company_symbol",outline,"ESG_approach_outline", list_type=True)
-point = 'some kind of point here'
-write_output("company_symbol",point,"ESG_approach",header=True) 
-article = "some article here"
-write_output("company_symbol",article,"ESG_approach")
